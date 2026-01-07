ade0d2146f5cbb822d6aa04eab93964f52824607
iiaydedeii <ahmetyasindd@gmail.com>
2026-01-07 09:50:21 +0300

    Completed memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,37 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/06 13:34:11 by adede             #+#    #+#             */
-/*   Updated: 2026/01/07 09:30:12 by adede            ###   ########.fr       */
+/*   Created: 2026/01/07 09:30:59 by adede             #+#    #+#             */
+/*   Updated: 2026/01/07 09:44:48 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <string.h>
-# include <unistd.h>
-# include <stdlib.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char	*dest_casted;
+	char	*src_casted;
 
-int		ft_isdigit(int c);
-
-int		ft_isalnum(int c);
-
-int		ft_isascii(int c);
-
-int		ft_isprint(int c);
-
-size_t	ft_strlen(const char *s);
-
-void	*ft_memset(void *s, int c, size_t n);
-
-void	ft_bzero(void *s, size_t n);
-
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-
-#endif
+	dest_casted = (char *)dest;
+	src_casted = (char *)src;
+	while (n > 0)
+	{
+		dest_casted[n - 1] = src_casted[n - 1];
+		n--;
+	}
+	return (dest);
+}
