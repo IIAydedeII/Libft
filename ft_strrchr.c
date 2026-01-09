83a2f997c5e79ee67fcbad0921af3f629a088c4d
IIAydedeII <ahmetyasindd@gmail.com>
2026-01-09 12:51:16 +0300

    Completed strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 15:09:29 by adede             #+#    #+#             */
+/*   Updated: 2026/01/08 10:04:28 by adede            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char *last_occurrence;
+
+	last_occurrence = NULL;
+	while (*s)
+	{
+		if (*s == c)
+			last_occurrence = (char *)s;
+		s++;
+	}
+	if (c == '\0')
+		last_occurrence = (char *)s;
+	return (last_occurrence);
+}
