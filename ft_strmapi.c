bffdd5b8396926653368da7c1ad6242b6956dae7
IIAydedeII <ahmetyasindd@gmail.com>
2026-01-18 19:41:05 +0300

    Completed ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/16 10:54:40 by adede             #+#    #+#             */
+/*   Updated: 2026/01/16 11:52:20 by adede            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	char	*map;
+	size_t	length;
+	size_t	i;
+
+	length = ft_strlen(s);
+	map = ft_calloc(length + 1, sizeof(char));
+	if (!map)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		map[i] = f(i, s[i]);
+		i++;
+	}
+	return (map);
+}
