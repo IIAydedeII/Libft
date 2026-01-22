c484c7c4493b22aff5176181cc6a24da4de2e06b
iiaydedeii <ahmetyasindd@gmail.com>
2026-01-22 10:03:35 +0300

    Fixed returning the first address instead of the last in ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 15:09:29 by adede             #+#    #+#             */
-/*   Updated: 2026/01/13 09:12:36 by adede            ###   ########.fr       */
+/*   Updated: 2026/01/22 10:01:51 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	if (c == '\0')
-		return ((char *)s);
 	while (*s)
 	{
 		if (*s == c)
 			return ((char *)s);
 		s++;
 	}
+	if (c == '\0')
+		return ((char *)s);
 	return (NULL);
 }
