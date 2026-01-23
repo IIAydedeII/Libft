7201f4d4ca5fd5a9bd60648790ee6344c407dae8
iiaydedeii <ahmetyasindd@gmail.com>
2026-01-23 12:40:05 +0300

    Improved strtrim to use existing functions from the library

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,21 +6,11 @@
 /*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 10:54:40 by adede             #+#    #+#             */
-/*   Updated: 2026/01/16 11:52:20 by adede            ###   ########.fr       */
+/*   Updated: 2026/01/23 12:37:40 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_includes(const char *set, const char c)
-{
-	while (*set)
-	{
-		if (*set++ == c)
-			return (1);
-	}
-	return (0);
-}
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
@@ -30,19 +20,16 @@ char	*ft_strtrim(const char *s1, const char *set)
 	size_t	length;
 
 	index_start = 0;
-	while (s1[index_start] && ft_includes(set, s1[index_start]))
+	while (s1[index_start] && ft_strchr(set, s1[index_start]))
 		index_start++;
 	index_end = ft_strlen(s1);
 	if (index_end > 0)
 		index_end--;
-	while (index_end > index_start && ft_includes(set, s1[index_end]))
+	while (index_end > index_start && ft_strchr(set, s1[index_end]))
 		index_end--;
 	length = 0;
 	if (index_end >= index_start)
 		length = index_end + 1 - index_start;
-	strtrim = ft_calloc(length + 1, sizeof(char));
-	if (!strtrim)
-		return (NULL);
-	ft_strlcpy(strtrim, s1 + index_start, length + 1);
+	strtrim = ft_substr(s1, index_start, length);
 	return (strtrim);
 }
