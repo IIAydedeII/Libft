3f080ac67064136a253b049d6d3204e42df00907
iiaydedeii <ahmetyasindd@gmail.com>
2026-01-23 13:07:30 +0300

    Optimized ft_split so that it doesn't use an extra function

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 10:54:40 by adede             #+#    #+#             */
-/*   Updated: 2026/01/19 09:15:33 by adede            ###   ########.fr       */
+/*   Updated: 2026/01/23 13:03:16 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,6 @@ static size_t	ft_count_words(const char *s, char c)
 	return (count);
 }
 
-static char	*ft_strldup(const char *s, size_t size)
-{
-	char	*dup;
-
-	dup = ft_calloc(size + 1, sizeof(char));
-	if (!dup)
-		return (NULL);
-	ft_memcpy(dup, s, size);
-	return (dup);
-}
-
 static void	ft_free_split(char **split, size_t i)
 {
 	while (i > 0)
@@ -68,7 +57,7 @@ char	**ft_split(const char *s, char c)
 			word_length = 0;
 			while (s[word_length] != c && s[word_length])
 				word_length++;
-			split[word_index] = ft_strldup(s, word_length);
+			split[word_index] = ft_substr(s, 0, word_length);
 			if (!split[word_index])
 				return (ft_free_split(split, word_index), NULL);
 			word_index++;
