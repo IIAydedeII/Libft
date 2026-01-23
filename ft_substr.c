aac59ac99c65474deb73307bf42ccb0168a13251
iiaydedeii <ahmetyasindd@gmail.com>
2026-01-23 17:29:29 +0300

    Prevented over-allocation in ft_substr by allocating no more than substring length

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 15:09:29 by adede             #+#    #+#             */
-/*   Updated: 2026/01/16 10:30:22 by adede            ###   ########.fr       */
+/*   Updated: 2026/01/23 17:25:07 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	char	*substr;
 	size_t	i;
 
+	if (start >= ft_strlen(s))
+		len = 0;
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
 	substr = ft_calloc(len + 1, sizeof(char));
 	if (!substr)
 		return (NULL);
-	if (start > ft_strlen(s))
-		return (substr);
 	i = 0;
-	while (len - i > 0 && s[start + i])
+	while (i < len)
 	{
 		substr[i] = s[start + i];
 		i++;
