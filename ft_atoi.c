ce781b52d4c8546467fe972ee8d6efae89536115
iiaydedeii <ahmetyasindd@gmail.com>
2026-01-23 17:04:45 +0300

    Fixed space char range condition for ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 14:18:34 by adede             #+#    #+#             */
-/*   Updated: 2026/01/13 14:21:02 by adede            ###   ########.fr       */
+/*   Updated: 2026/01/23 16:00:18 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_atoi(const char *nptr)
 
 	number = 0;
 	sign = 1;
-	while (*nptr == ' ' || (*nptr == '\t' && *nptr == '\r'))
+	while (*nptr == ' ' || ('\t' <= *nptr && *nptr <= '\r'))
 		nptr++;
 	if (*nptr == '+' || *nptr == '-')
 	{
