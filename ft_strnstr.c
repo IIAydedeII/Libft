18bd02859613ff5db52fa414617f126f5aadfcb7
iiaydedeii <ahmetyasindd@gmail.com>
2026-01-28 13:51:04 +0300

    Simplified ft_strnstr by using ft_strncmp

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 15:09:29 by adede             #+#    #+#             */
-/*   Updated: 2026/01/23 15:57:28 by adede            ###   ########.fr       */
+/*   Updated: 2026/01/28 13:48:35 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	c;
-	size_t	c2;
+	size_t	little_len;
 
-	c = 0;
-	if (ft_strlen(little) == 0)
+	little_len = ft_strlen(little);
+	if (little_len == 0)
 		return ((char *)big);
-	while (big[c] && len > 0)
+	c = 0;
+	while (big[c] && c + little_len <= len)
 	{
-		c2 = 0;
-		while (big[c + c2] == little[c2] && c + c2 < len && little[c2])
-			c2++;
-		if (little[c2] == '\0')
+		if (ft_strncmp(&big[c], little, little_len) == 0)
 			return ((char *)&big[c]);
 		c++;
 	}
